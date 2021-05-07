9715c9f3be3351eb657a816a8fc084e849b444d5
melon-husk <ujwal5555@gmail.com>
2021-05-07 08:02:15 +0530

    added function to get file path using IPC

---
+++ src/GetFilePath/getFilePath.tsx
@@ -0,0 +1,25 @@
+import { ipcRenderer } from 'electron';
+import constants from '../utils/constants';
+// const electron = require('electron');
+
+type FilePath = [string];
+
+const getFilePath = (): Promise<FilePath> => {
+  return new Promise((resolve, reject) => {
+    ipcRenderer.send(constants.GET_DIRECTORY_PATH);
+    ipcRenderer.on(constants.SEND_DIRECTORY_PATH, (_event, args) => {
+      if (args.length === 0) reject(new Error('Directory is empty'));
+      resolve(args);
+    });
+  });
+};
+
+export default getFilePath;
+
+// return new Promise((resolve, reject) => {
+
+//   dialog
+//     .showOpenDialog({ properties: ['openDirectory'] })
+//     .then((output) => resolve(output.filePaths))
+//     .catch((error) => reject(error));
+// });
